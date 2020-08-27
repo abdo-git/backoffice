20f3a4150877bdef775c7514b3e7e89776a927a2
abaddi <53653934+abdo-github@users.noreply.github.com>
2020-08-27 16:31:02 +0200

    Revert "add import"

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React,{component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
